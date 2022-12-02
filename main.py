af326a55f0d44b31a0aaaafd97f1cf0234ebd7d9
student <Shonowin@gmail.com>
2022-12-02 15:12:32 +0300

    IS_READY

---
+++ main.py
@@ -0,0 +1,12 @@
+#function is very important!
+def superfunction():
+    print('Привет питон')
+def main():
+    print('Первый принт')
+    print('Второй принт')
+    print('Третий принт')
+    print('Четвертый принт')
+if __name__ == '__main__':
+    main()
+
+print('1')
